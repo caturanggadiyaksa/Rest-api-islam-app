807ec56f4e14f50bba045fbbc374d7434bb872ab
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-10-01 08:30:26 +0700

    add field table surah

--- database/migrations/2023_09_30_105109_create_surah_table.php
+++ database/migrations/2023_09_30_105109_create_surah_table.php
@@ -15,6 +15,19 @@ class CreateSurahTable extends Migration
     {
         Schema::create('surah', function (Blueprint $table) {
             $table->id();
+            $table->integer('number');
+            $table->integer('sequence');
+            $table->integer('numberOfVerses');
+            $table->string('shortName');
+            $table->string('longName');
+            $table->string('transliteration_en');
+            $table->string('transliteration_id');
+            $table->string('translation_en');
+            $table->string('translation_id');
+            $table->string('revelation_arab');
+            $table->string('revelation_en');
+            $table->string('revelation_id');
+            $table->text('tafsir_id');
             $table->timestamps();
         });
     }
